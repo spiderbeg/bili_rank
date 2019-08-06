e790b8e04e28aeffad3f632c470c774f9ecac2de
spiderbeg <3368483402@qq.com>
2019-08-06 16:33:25 +0800

    change log level

--- get_rank.py
+++ get_rank.py
@@ -2,13 +2,14 @@
 # blbl 粉丝前一百检查
 import requests
 import time
+from datetime import datetime
 import copy
 #外部启动django脚本-----------------------------------------------------
 import os 
 import sys
 import django
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 # 将django项目根目录加入环境变量 # 当在 django 文件夹下时，就已经有了 python 的环境变量。不在 django 文件夹下使用这句
 # sys.path.extend([r'E:\CS\CS_Projects\pythonv\Web_Projects\rank',])# python 的环境变量 # 
@@ -83,7 +84,7 @@ def get_new(send_count):
                         send_count.append(se) # 添加元素
                         send_count = sorted(send_count, key=lambda item:item[1]) # 排序
 
-    logging.debug('更新后元组%s',send_count)
+    logging.warning('更新后元组%s',send_count)
     newz = {} # 新的排名
     for i,send in enumerate(send_count):
         send[1][2] = 100-i   # 加排名
@@ -98,14 +99,14 @@ for a in al:
 
 send_count = sorted(oldz.items(), key=lambda item:item[1][0]) # 生成元组中的列表还是字典中的列表
 send_count = copy.deepcopy(send_count) # 深复制
-logging.debug('输入的send_count%s',send_count)
+logging.warning('输入的send_count%s',send_count)
 start = time.time()
 newz = get_new(send_count)
 end = time.time()
 use = end - start
-logging.debug('原来的排名情况%s', oldz)
-logging.debug('此次更新花费时间/分%s', use/60)
-logging.debug('新的排名情况%s', newz)
+logging.warning('原来的排名情况%s', oldz)
+logging.warning('此次更新花费时间/分%s', use/60)
+logging.warning('新的排名情况%s', newz)
 
 # 2、 将排名数据更新至数据库
 usez = copy.deepcopy(newz)
@@ -118,6 +119,6 @@ for i in newz:
         crank = -200
     usez[i].extend([cfollower,crank])
         
-logging.debug('最后储存的排名情况%s', usez)
+logging.warning('时间：%s, 最后储存的排名情况%s', datetime.now(), usez)
 for z in usez:
     obj = models.User_info.objects.create(uid=z, name=usez[z][1], follower_count=usez[z][0], rank=usez[z][2], addfollower=usez[z][3], lastrank=usez[z][4]) # update
